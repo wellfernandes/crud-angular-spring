a67e71d159cab2aad8c46a368e58c66136c1c5e4
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-30 17:06:11 -0300

    courses module with routing added

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoursesRoutingModule } from './courses-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CoursesRoutingModule
   ]
 })
 export class CoursesModule { }
