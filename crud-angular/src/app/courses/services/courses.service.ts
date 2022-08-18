c85587eb368c3dab3e820519233efb79723a784a
Welliton Fernandes Leal <wellfernandes@me.com>
2022-08-18 15:41:29 -0300

    http get call in angular and rxjs

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Course } from './../model/course';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  findAll() : Course[] {
-    return [
-      {_id: '1', name: 'Angular', category: 'front-end'}
-    ];
+  findAll() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+    );
   }
 }
